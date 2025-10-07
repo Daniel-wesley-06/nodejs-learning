27e4d56547e2285dcc7461404afc5dbcf974acea
Danielwesley-2006 <wesley.utukuru@gmail.com>
2025-10-07 10:00:21 +0530

    Initial commit

---
+++ modules.js
@@ -0,0 +1,12 @@
+// Modules
+// CmmonJS,  every file is a module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-flavour')
+// console.log(names)
+// sayHi(names.john)
+// sayHi(names.peter)
+// console.log(data)
+require('./mind-grenade')
